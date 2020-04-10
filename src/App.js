34411526c8a0b08baf40d8ce03dc9ea883ce720a
Katajisto <t@ktj.st>
2020-04-11 01:33:49 +0300

    Made MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const linkList = [
+    {
+      title: "CSS tricks: complete guide to flexbox.",
+      link: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+    },
+    {
+      title: "Emmet cheatsheet, VS code",
+      link: "https://docs.emmet.io/cheat-sheet/",
+    },
+  ];
+
+  const [test, setTest] = useState("");
+
+  function onInput(e) {
+    setTest(e.target.value);
+  }
+
+  function containsKeyword(linkTitle) {
+    return linkTitle.title.toLowerCase().includes(test.toLowerCase());
+  }
+
+  const renderList = linkList.filter(containsKeyword).map((x) => (
+    <div>
+      <a href={x.link} className="list-a">
+        {x.title}
+      </a>
+    </div>
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">
+        <i className="fas fa-code"></i> Developer link collection
+      </h1>
+      <h2 className="app-subtitle">devlinks.ktj.st</h2>
+      <div className="input-container">
+        <div>
+          <i className="fas fa-search search-icon"></i>
+          <input
+            className="app-search-input"
+            type="text"
+            placeholder="What do you seek?"
+            value={test}
+            onChange={onInput}
+          ></input>
+        </div>
+      </div>
+      <div className="link-list">{renderList}</div>
     </div>
   );
 }
